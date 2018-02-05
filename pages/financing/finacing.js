22367e3bf6e5e8ee157ed2ad2a6e104d8f211a34
gexingqiao <517839371@qq.com>
2018-02-05 10:39:01 +0800

    upload

---
+++ pages/financing/finacing.js
@@ -0,0 +1,107 @@
+// pages/financing/finacing.js
+var list=[];
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      { name: "新华恒生村镇银行001号理财",yields:5.05,date:6,limit:"10万起购"}, 
+      { name: "新华恒生村镇银行002号理财",yields:5.10,date:7,limit:"5万起购"},
+      { name: "新华恒生村镇银行003号理财",yields:5.20,date:8,limit:"20万起购"},
+      { name: "新华恒生村镇银行004号理财",yields:5.22,date:9,limit:"30万起购"},
+      { name: "新华恒生村镇银行005号理财",yields:5.32,date:10,limit:"40万起购"}
+    ],
+    productList:[]
+  },
+
+  //页面底部刷新事件
+  lower:function(e){
+    console.log(e);
+  },
+
+  //列表排序方法
+  sort:function(e){
+    var sortList=this.data.list;
+    console.log(sortList);
+    if(e.target.id==="y"){
+      console.log(e);
+      for (var i = 0; i < sortList.length-1;i++){
+        for (var j = 0; j < sortList.length-1-i;j++){
+          if (sortList[j].yields < sortList[j+1].yields){
+            var temp = sortList[j];
+            sortList[j] = sortList[j+1];
+            sortList[j+1]=temp;
+          }
+        }
+      }
+    }
+    console.log(sortList);
+    this.setData({
+      list:sortList
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      productList:list
+    })
+    //更改页面标题
+    wx.setNavigationBarTitle({
+      title: "理财产品"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,70 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    financing:{
+      avatarUrl:'../image/理财.jpg',
+      nickName:'理财产品'
+    },
+    station:{
+      avatarUrl:'../image/网点.jpg',
+      nickName: '网点查询'
+    },
+    creditCard:{
+      avatarUrl:'../image/信用卡.jpg',
+      nickName: '信用卡申请'
+    },
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  ToFinancingPage:function(){
+    wx.navigateTo({
+      url: '../financing/finacing'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
